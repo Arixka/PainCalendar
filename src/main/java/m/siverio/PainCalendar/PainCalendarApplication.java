c2bc9a6037117a93c831fef20ed9a1d3d8b24e76
Arixka <marisiver25@gmail.com>
2025-09-21 20:25:01 +0100

    Initial commit

---
+++ src/main/java/m/siverio/PainCalendar/PainCalendarApplication.java
@@ -0,0 +1,21 @@
+package m.siverio.PainCalendar;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PainCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PainCalendarApplication.class, args);
+
+		System.out.println("*******");
+	}
+	@PostConstruct
+	public void checkEnv() {
+		System.out.println("DB_URL: " + System.getenv("DB_URL"));
+		System.out.println("DB_USER: " + System.getenv("DB_USER"));
+		System.out.println("DB_PASSWORD: " + System.getenv("DB_PASSWORD"));
+	}
+}
---
+++ src/test/java/m/siverio/AuthMaster/PainCalendarAppTests.java
@@ -0,0 +1,13 @@
+package m.siverio.AuthMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PainCalendarAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
